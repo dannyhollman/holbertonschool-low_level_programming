3707e36e01633e8a4de85786901b4aa5eadcddad
Danny <dannyhollman@gmail.com>
2019-06-19 16:58:14 +0000

    Update file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main ()
+/**
+ * main - Print string
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

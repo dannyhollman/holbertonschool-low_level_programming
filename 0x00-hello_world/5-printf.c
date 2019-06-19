aad6d4bd97297fbfdb0a0fe964b06f6eae1eea31
Danny <dannyhollman@gmail.com>
2019-06-19 17:01:51 +0000

    Add file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print string
+ *
+ * Return: Always 0 (success)
+ */
+int main()
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return 0;
+}

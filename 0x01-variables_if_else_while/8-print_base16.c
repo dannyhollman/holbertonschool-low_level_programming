6c4f19d15eed14baf82cef1917aaa8e439eabc0d
Danny <dannyhollman@gmail.com>
2019-06-20 21:17:31 +0000

    Add file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char letter;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

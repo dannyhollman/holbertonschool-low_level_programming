a08ca7290999dd1bf7aff3d6497c8aa269dac4a2
Danny <dannyhollman@gmail.com>
2019-06-21 00:14:29 +0000

    Update file 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		putchar((i/10)+'0');
-		putchar((i%10)+'0');
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 		if (i != 99)
 		{
 			putchar(',');

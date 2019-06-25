e7dd917c42a969a20c6c8be7854943134225193e
Danny <dannyhollman@gmail.com>
2019-06-25 01:04:40 +0000

    Add file 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  * main - Check if char is lowercase
+  *
+  * Return: 1 if lowercase, 0 otherwise
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
+

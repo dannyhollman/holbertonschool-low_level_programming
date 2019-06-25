a43d441bfe5909d997677e8461cd1073130cbf35
Danny <dannyhollman@gmail.com>
2019-06-25 17:53:56 +0000

    Add file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+  * add - adds two ints
+  *
+  * Return: sum of ints
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * times_tables - prints 0-9 times table
+  *
+  * Return: void
+  */
+void times_table(void)
+{
+	int i;
+
+	int n;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (n = 0; n < 9 

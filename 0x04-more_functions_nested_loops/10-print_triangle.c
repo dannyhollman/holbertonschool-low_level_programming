b8b7bed2d543d827ac735aceedfd07f26919b3e2
Danny <dannyhollman@gmail.com>
2019-06-27 16:59:32 +0000

    Add file 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,31 @@
   */
 void print_triangle(int size)
 {
-	int i;
+	int col, row;
+	
+	int place = size - 1;
 
+	if ( size > 0)
+	{
+		for (col = 0; col < size; col++)
+		{
+			for (row = 0; row < size; row++)
+			{
+				if (row < place)
+				{
+					_putchar(' ');
+				}
+				else if (row >= place)
+				{
+					_putchar('#');
+				}
+			}
+		place--;
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

08f3dcdda318bf603f60407e081c0476150df7b3
Danny <dannyhollman@gmail.com>
2019-06-28 01:33:38 +0000

    Update file 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,41 +8,28 @@
   */
 void print_number(int n)
 {
+	int divide = 1;
+
+	int count = 1;
+	
+	/* convert if negative and print '-' */
 	if (n < 0)
-	{	_putchar('-'); }
-	if ((n < 9 && n >= 0) || (n < 0 && n > -9))
 	{
-		if (n < 0)
-		{	n *= -1; }
-		_putchar((n) + '0'); }
-	else if ((n > 9 && n  < 100) || (n < -9 && n > -100))
+		n *= -1;
+		_putchar('-');
+	}
+	/* dynamically gets digit count and number to divide by */
+	while (divide <= n / 10)
 	{
-		if (n < 0)
-		{	n *= -1; }
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0'); }
-	else if ((n > 99 && n  < 1000) || (n < -99 && n > -1000))
+		count++;
+		divide *= 10;
+	}
+	/* prints digit from left to right */
+	while (count > 0)
 	{
-		if (n < 0)
-		{	n *= -1; }
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0'); }
-	else if ((n > 999 && n  < 10000) || (n < -999 && n > -10000))
-	{
-		if (n < 0)
-		{	n *= -1; }
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0'); }
-	else if ((n > 9999 && n < 100000) || (n < -9999 && n > -100000))
-	{
-		if (n < 0)
-		{	n *= -1; }
-		_putchar((n / 10000) + '0');
-		_putchar(((n / 1000) % 10) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0'); }
+		_putchar((n / divide) + '0');
+		n %= divide;
+		divide /= 10;
+		count--;
+	}
 }

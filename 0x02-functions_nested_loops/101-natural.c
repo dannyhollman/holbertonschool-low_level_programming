8867f729b1012e2fbc3febc1be9a9127c32aeac8
Danny <dannyhollman@gmail.com>
2019-06-30 17:52:54 +0000

    Update file 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * main - prints sum of all multiples of 3 or 5 below 1024
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	int i;
+
+	int total = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			total += i;
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}

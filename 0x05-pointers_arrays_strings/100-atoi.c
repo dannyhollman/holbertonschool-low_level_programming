922489293c5c2f8bc040ef5076f9063d057e2ffe
Danny <dannyhollman@gmail.com>
2019-07-02 20:48:29 +0000

    Add file 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  *
+  *
+  *
+  */
+int _atoi(char *s)
+{
+	int count = 0, loop, digitCount = 0, neg = 1, start = 0, multiple = 1; 
+
+	int temp, number = 0;
+
+	for (loop = 0; s[loop] != '\0'; loop++)
+	{
+		if (s[loop] == '-')
+			neg *= -1;
+		if (s[loop] >= 48 && s[loop] <= 57)
+		{
+			digitCount++;
+			if (digitCount > 1)
+				multiple *= 10;
+			if (!(s[loop + 1] >= 48 && s[loop + 1] <= 57))
+				break;
+		}
+	}
+	for (start = (loop + 1) - digitCount; start < loop + 1; start++)
+	{
+		temp = (s[start] - '0') * multiple;
+		number += temp;
+		multiple /= 10;
+	}
+	number *= neg;
+	return(number);
+}

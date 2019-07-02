d3ad23a15c0801cd0ed0410d4c6e181c5fb0a7ca
Danny <dannyhollman@gmail.com>
2019-07-02 14:50:12 +0000

    Add file 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,25 @@
 #include "holberton.h"
 /**
+  * _strcpy - copy string 
   *
+  * @dest: destination
   *
+  * @src: source
   *
+  * Return: copied string
   */
 char *_strcpy(char *dest, char *src)
 {
-	
+	int count = 0, loop;
+
+	while (src[count] != '\0')
+	{
+		count++;
+	}
+
+	for (loop = 0; loop <= count; loop++)
+	{
+		dest[loop] = src[loop];
+	}
+	return (dest);
+}

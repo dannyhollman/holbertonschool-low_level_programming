d233b525e121688f1c23fe7043516d20962d8536
Danny <dannyhollman@gmail.com>
2019-07-06 16:50:57 +0000

    Update format file 2,3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
 /**
+  * _strncpy - copies a string
   *
+  * @dest: destination string
   *
+  * @src: source string
   *
+  * @n: number of bytes to copy
+  *
+  * Return: pointer to copied string
   */
 char *_strncpy(char *dest, char *src, int n)
 {
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
+  * _strcmp - compares two strings
   *
+  * @s1: first string to compare
   *
+  * @s2: second string to compare
+  *
+  * Return: 0 if strings match, positive if s1 bigger, negative if s1 smaller
   */
 int _strcmp(char *s1, char *s2)
 {
@@ -18,6 +23,6 @@ int _strcmp(char *s1, char *s2)
 		}
 	}
 	retrn = first - second;
-	
+
 	return (retrn);
 }

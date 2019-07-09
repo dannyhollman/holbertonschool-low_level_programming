8fe59970a0fbd5a06f7c443b8540bd63c4c98010
Danny <dannyhollman@gmail.com>
2019-07-09 17:53:52 +0000

    Update file 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,11 @@ char *_strchr(char *s, char c)
 {
 	char *loop = s;
 
-	char *nope = "NULL";
-
 	while (*loop)
 	{
 		if (*loop == c)
 			return (loop);
 		loop++;
 	}
-	return (nope);
+	return (loop);
 }

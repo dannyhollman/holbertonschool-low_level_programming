9df8d7c28d2f1d01e24960e5d9e527dbbfbe6ab0
Danny <dannyhollman@gmail.com>
2019-07-09 18:30:54 +0000

    Update file 2,4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
   * _strchr - locates character in string
   * @s: string to search
@@ -15,5 +16,5 @@ char *_strchr(char *s, char c)
 			return (loop);
 		loop++;
 	}
-	return (loop);
+	return (0);
 }

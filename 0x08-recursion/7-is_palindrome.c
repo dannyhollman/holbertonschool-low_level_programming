422e67b6f57d58cc96b903544b52701fcad4a687
Danny <dannyhollman@gmail.com>
2019-07-10 23:34:33 +0000

    Add file 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * helper - recursive helper function
+  * @s: input string
+  * @start: starting index
+  * @end: ending index
+  * Return: recursively returns function
+  */
+int helper(char *s, int start, int end)
+{
+	if (s[start] == s[end])
+	{
+		if (end == 1)
+			return (1);
+		else
+			return (helper(s, ++start, --end));
+	}
+	return (0);
+}
+/**
+  * is_palindrome - checks if string is palindrome
+  * @s: input string
+  * Return: 1 if palendrone, 0 otherwise
+  */
+int is_palindrome(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+	{
+		count++;
+	}
+
+	count -= 1;
+	return (helper(s, 0, count));
+}

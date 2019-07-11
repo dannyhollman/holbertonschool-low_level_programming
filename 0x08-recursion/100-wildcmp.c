608d869e4f3a22d2e1f36c2436411e5d396e9f4e
Danny <dannyhollman@gmail.com>
2019-07-11 03:38:00 +0000

    Update file 100

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
-  *
-  *
-  *
+  * wildcard - deals with wildcard in string
+  * @s1: string 1
+  * @s2: string 2
+  * Return: 1 or 0
   */
 int helper(char *s1, char *s2);
-int wildcard (char *s1, char *s2)
+int wildcard(char *s1, char *s2)
 {
 	if (*s2 == '*')
 	{
@@ -20,6 +21,12 @@ int wildcard (char *s1, char *s2)
 	return (helper(s1, s2));
 	return (0);
 }
+/**
+  * helper - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  * Return: 1 or 0
+  */
 int helper(char *s1, char *s2)
 {
 	if (*s1 && *s2)
@@ -33,6 +40,12 @@ int helper(char *s1, char *s2)
 	}
 	return (1);
 }
+/**
+  * wildcmp - compares two strings with wildcards
+  * @s1: string 1
+  * @s2: string 2
+  * Return: 1 or 0
+  */
 int wildcmp(char *s1, char *s2)
 {
 	return (helper(s1, s2));

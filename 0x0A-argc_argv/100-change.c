95b575ae9a93f82cfa64e9518c89afb02cd752db
Danny <dannyhollman@gmail.com>
2019-07-13 16:23:16 +0000

    Update file 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -32,10 +32,15 @@ int main(int argc, char *argv[])
 {
 	int coins;
 
-	if (argc == 2)
+	if (argc == 2 && atoi(argv[1]) > 0)
 	{
 		coins = (counter(atoi(argv[1]), 0));
 		printf("%d\n", coins);
+		return (0);
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
 		return (0);
 	}
 	else

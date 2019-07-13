c6bdd8a194159a23d24b41d022c290753e658b85
Danny <dannyhollman@gmail.com>
2019-07-13 16:01:44 +0000

    Update file 0,1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,11 @@
   * main - prints program name
   * @argc: number of arguments
   * @argv: array of arguments
-  * Return: argc to avoid unused argument error
+  * Return: always 0
   */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }

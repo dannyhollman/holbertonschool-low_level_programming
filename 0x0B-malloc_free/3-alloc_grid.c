4f00d8e1d8c059817f774ffc799d951d9de7f2b5
Danny <dannyhollman@gmail.com>
2019-07-16 21:34:15 +0000

    Uddate file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
-  *
+  * alloc_grid - creates 2d array of integers
+  * @width: width of array
+  * @height: height of array
+  * Return: piinter to 2d array
   */
 int **alloc_grid(int width, int height)
 {
@@ -20,6 +20,10 @@ int **alloc_grid(int width, int height)
 	for (loop = 0; loop < width; loop++)
 	{
 		array[loop] = malloc(width * sizeof(int));
+		if (array[loop] == NULL)
+		{
+			return (NULL);
+		}
 	}
 
 	return (array);

10cd51cda64439705c1f7d88ea0d6bc001bc876c
Danny <dannyhollman@gmail.com>
2019-07-17 03:33:06 +0000

    Update file 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -7,15 +7,55 @@
   *
   *
   */
+int count(char *s)
+{
+	int count = 0;
+
+	char *tmp = s;
+
+	while (*tmp)
+	{
+		count++;
+		tmp++;
+	}
+	return (count);
+}
 char *argstostr(int ac, char **av)
 {
+	int loop = 0, loop2 = 0, total = 0, place = 0;
 
+	char *array;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-	printf("%s\n", av);
-	return NULL;
+	while (loop < ac)
+	{
+		total += count(av[loop]);
+		loop++;
+	}
+
+	loop = 1;
+
+	array = malloc(total * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	while (av[loop][loop2])
+	{
+		while (av[loop][loop2])
+		{
+			array[place] = av[loop][loop2];
+			loop2++;
+		}
+	printf("\n");
+	loop++;
+	loop2 = 0;
+	}
+	return (array);
 }

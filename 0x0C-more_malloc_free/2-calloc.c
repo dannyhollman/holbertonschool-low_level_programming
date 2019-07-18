a4c038a84ad889db585f52e0f1d533e56cf95213
Danny <dannyhollman@gmail.com>
2019-07-18 19:37:00 +0000

    Add file 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * _calloc - allocate memory for an array
+  * @nmemb: number of elements in array
+  * @size: size of elements in array
+  * Return: void pointer to allocated memy
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int loop;
+
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (loop = 0; loop <= nmemb; loop++)
+	{
+		ptr[loop] = 0;
+	}
+
+	return (ptr);
+}

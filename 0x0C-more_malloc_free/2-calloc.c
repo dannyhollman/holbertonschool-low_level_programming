c70578f7562ae9df1f676bc3156f6e87994338d3
Danny <dannyhollman@gmail.com>
2019-07-18 21:03:58 +0000

    Upate file 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (loop = 0; loop <= nmemb * size; loop++)
+	for (loop = 0; loop < nmemb * size; loop++)
 	{
 		((char *)ptr)[loop] = 0;
 	}

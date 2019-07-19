2e08a3d93b2216f1e719f460e92fb371a410c371
Danny <dannyhollman@gmail.com>
2019-07-19 18:02:06 +0000

    Update file 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	char *copy = ptr;
 
-	if (new_size == old_size)
+	if (new_size == old_size )
 	{
 		return (ptr);
 	}
@@ -25,6 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
+		return (ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{

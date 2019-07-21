4f0f3ac8d40d756cd840695ffdffd6667d3fd784
Danny Hollman <dannyhollman@gmail.com>
2019-07-21 19:09:40 +0000

    Update file 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,60 +2,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-  *
-  *
-  *
-  *
+  * count - finds length of string
+  * @s: input string
+  * Return: string length
   */
 int count(char *s)
 {
 	int count = 0;
 
-	char *tmp = s;
-
-	while (*tmp)
-	{
+	while (s[count])
 		count++;
-		tmp++;
-	}
-	return (count);
+	return (count + 1);
 }
+/**
+  * argstostr - concatenates all arguments
+  * @ac: number of arguments
+  * @av: array of arguments
+  * Return: pointer to concatenated string
+  */
 char *argstostr(int ac, char **av)
 {
-	int loop = 0, loop2 = 0, total = 0, place = 0;
+	int loop = 0, loop2 = 0, place = 0, size = 0;
 
 	char *array;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
 
-	while (loop < ac)
+	while (av[loop])
 	{
-		total += count(av[loop]);
+		size += count(av[loop]);
 		loop++;
 	}
-
-	loop = 1;
-
-	array = malloc(total * sizeof(char));
-
+	array = malloc(size + 1);
 	if (array == NULL)
-	{
 		return (NULL);
-	}
-
-	while (av[loop][loop2])
+	loop = 0;
+	while (av[loop])
 	{
+		if (loop != 0)
+		{
+			array[place] = '\n';
+			place++;
+		}
 		while (av[loop][loop2])
 		{
 			array[place] = av[loop][loop2];
 			loop2++;
+			place++;
 		}
-	printf("\n");
-	loop++;
-	loop2 = 0;
+		loop2 = 0;
+		loop++;
 	}
+	array[place] = '\n';
+
+
 	return (array);
 }

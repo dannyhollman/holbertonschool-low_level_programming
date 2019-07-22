9e2e8c4b19a0b1e117da7609ab64c37aea0dc170
Danny Hollman <dannyhollman@gmail.com>
2019-07-22 22:35:56 +0000

    Update file 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,17 +12,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new;
 
 	new = malloc(sizeof(dog_t));
-	
 	if (new == NULL)
 		return (NULL);
-	
-
 
 	new->name = malloc(sizeof(name));
-	new->age = age;
+	if (new->name == NULL)
+		return (NULL);
+
 	new->owner = malloc(sizeof(owner));
+	if (new->owner == NULL)
+		return (NULL);
 
 	new->name = name;
+	new->age = age;
 	new->owner = owner;
 
 	return (new);

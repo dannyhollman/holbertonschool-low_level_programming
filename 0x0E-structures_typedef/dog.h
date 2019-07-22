f7a2a3deecf0d42cb65fa175c21c256b46979dc6
Danny Hollman <dannyhollman@gmail.com>
2019-07-22 16:43:15 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+  * struct dog - struct with name, age, and owner
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owners name
+  *
+  * Description: Struct includes name of dog, age, name of owner
+  */
 struct dog
 {
 	char *name;

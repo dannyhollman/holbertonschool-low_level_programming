4302c77ea48668645a6c975f4aef1f20c156d390
Danny Hollman <dannyhollman@gmail.com>
2019-07-23 00:00:21 +0000

    Update file 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strdup - copies string to newly allocated space
+ * @str: input string
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+  int count = 0, loop = 0;
+
+  char *array;
+
+  if (str == NULL)
+      return (NULL);
+
+  while (str[count])
+      count++;
+
+  count++;
+
+  array = malloc(count * sizeof(char));
+
+  if (array == NULL)
+      return (NULL);
+
+  while (loop < count)
+    {
+      array[loop] = str[loop];
+      loop++;
+    }
+  return (array);
+}
+/**
   * new_dog - creates new dog struct
   * @name: name of dog
   * @age: age of dog
@@ -16,25 +47,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	if (name != NULL)
+
+	new->name = _strdup(name);
+	if (new->name == NULL)
 	{
-		new->name = name;
-		if (new->name == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
+		free(new);
+		return (NULL);
 	}
 
-	if (owner != NULL)
+	new->owner = _strdup(owner);
+	if (new->owner == NULL)
 	{
-		new->owner = owner;
-		if (new->owner == NULL)
-		{
-			free(new->name);
-			free(new);
-			return (NULL);
-		}
+		free(new->name);
+		free(new);
+		return (NULL);
 	}
 
 	new->age = age;

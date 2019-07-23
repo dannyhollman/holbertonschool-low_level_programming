8f7a42325d8b0f5d48ea6caa7521f65490cbb2c7
Danny Hollman <dannyhollman@gmail.com>
2019-07-23 00:04:45 +0000

    Finally fixed 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,29 +9,29 @@
  */
 char *_strdup(char *str)
 {
-  int count = 0, loop = 0;
+	int count = 0, loop = 0;
 
-  char *array;
+	char *array;
 
-  if (str == NULL)
-      return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-  while (str[count])
-      count++;
+	while (str[count])
+		count++;
 
-  count++;
+	count++;
 
-  array = malloc(count * sizeof(char));
+	array = malloc(count * sizeof(char));
 
-  if (array == NULL)
-      return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-  while (loop < count)
-    {
-      array[loop] = str[loop];
-      loop++;
-    }
-  return (array);
+	while (loop < count)
+	{
+		array[loop] = str[loop];
+		loop++;
+	}
+	return (array);
 }
 /**
   * new_dog - creates new dog struct

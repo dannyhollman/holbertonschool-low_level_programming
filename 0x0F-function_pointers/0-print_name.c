b3175d1b22c0a17d5c10af4a66b7821066a0883a
Danny Hollman <dannyhollman@gmail.com>
2019-07-24 13:42:46 +0000

    Add file 0, header

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+  * print_name - calls a function to print a name
+  * @name: input name
+  * @f: function to print name
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

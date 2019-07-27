8264768a9d04b2a609ac25af308ee5ee925420a7
Danny Hollman <dannyhollman@gmail.com>
2019-07-27 16:30:13 +0000

    Update file 1, betty format

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,9 +1,9 @@
 #include "stdarg.h"
 #include <stdio.h>
 /**
-  *
-  *
-  *
+  * print_numbers - prints numbers
+  * @separator: number separator
+  * @n: number of inputs
   */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)
+		if (i != 0 && separator != '\0')
 			printf("%s", separator);
 		printf("%d", va_arg(valist, int));
 	}

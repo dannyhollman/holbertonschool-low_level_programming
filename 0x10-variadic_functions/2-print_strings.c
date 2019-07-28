df8c7337153aea5958ae31ef07a3a4dac029149d
Danny Hollman <dannyhollman@gmail.com>
2019-07-28 13:17:44 +0000

    [LOCAL][UPDATE] Fix output for NULL string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,13 +1,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
-  *
-  *
-  *
+  * print_strings - prints strings followed by new line
+  * @separator: string separator
+  * @n: number of arguments
   */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+
+	char *temp;
 
 	va_list valist;
 
@@ -15,9 +17,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)
+		temp = va_arg(valist, char *);
+		if (i != 0 && separator != '\0')
 			printf("%s", separator);
-		printf("%s", va_arg(valist, char *));
+		if (temp == NULL)
+			printf("(nil)");
+		else
+			printf("%s", temp);
 	}
 	printf("\n");
 }

afd73d38ccd15984392d8bcf7c748925ddd1dcb8
Danny Hollman <dannyhollman@gmail.com>
2019-08-02 02:57:44 +0000

    [LOCAL][INIT] Add file 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  * free_list - free all elements of linked list
+  * @head: pointer to head of list
+  */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	list_t *current = head;
+
+	while (current->next != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+	free(current->str);
+	free(current);
+}

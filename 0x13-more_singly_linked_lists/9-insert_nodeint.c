79e8e35b61edc4c3c8fb492a157ed9a0107a1f5b
Danny Hollman <dannyhollman@gmail.com>
2019-08-06 15:35:14 +0000

    [LOCAL][UPDATE] Remove unnecessary NULL check, clean up code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,11 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *new_node, *current;
 
-	if (*head == NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
@@ -29,18 +25,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (*head);
 	}
-	while (loop < idx - 1 && current->next != NULL)
+	while (current != NULL)
 	{
+		if (loop == idx - 1)
+		{
+			new_node->n = n;
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
 		current = current->next;
 		loop++;
 	}
-
-	if (loop != idx - 1)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+	return (NULL);
 }

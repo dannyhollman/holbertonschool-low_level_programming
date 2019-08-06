8cc62940b461d5f29ec854216b0f9dbefd280653
Danny Hollman <dannyhollman@gmail.com>
2019-08-06 15:08:01 +0000

    [LOCAL][UPDATE] Fix index 0 not working

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (loop < idx - 1 && current->next != NULL)
 	{
 		current = current->next;

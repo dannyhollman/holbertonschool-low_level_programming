b20a99d9844080ad4bd1a968964b092ec7619b42
Danny Hollman <dannyhollman@gmail.com>
2019-08-06 15:16:04 +0000

    [LOCAL][UPDATE] Change return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
-		return (new_node);
+		return (*head);
 	}
 	while (loop < idx - 1 && current->next != NULL)
 	{

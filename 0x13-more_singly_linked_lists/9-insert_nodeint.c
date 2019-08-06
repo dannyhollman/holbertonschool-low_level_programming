2149a9df6b45e3a08be1e87d411504631b42e391
Danny Hollman <dannyhollman@gmail.com>
2019-08-06 12:35:55 +0000

    [LOCAL][UPDATE] Check if index is in range

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,11 +22,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 
-	while (loop < idx - 1)
+	while (loop < idx - 1 && current->next != NULL)
 	{
 		current = current->next;
 		loop++;
 	}
+
+	if (loop != idx - 1)
+		return (NULL);
 
 	new_node->n = n;
 	new_node->next = current->next;

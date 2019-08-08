1666c2caa3558cb95fb94a3bcb74ed51ad46dd2b
Danny Hollman <dannyhollman@gmail.com>
2019-08-08 18:35:19 +0000

    [LOCAL][UPDATE] Use bitwise instead of % and /

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,35 @@
 #include "holberton.h"
 /**
- * to_binary - converts decimal to binary
- * @n: input number
- */
-void to_binary(unsigned long int n)
-{
-	if (!n)
-		return;
-	to_binary(n / 2);
-	_putchar(n % 2 + '0');
-}
-/**
  * print_binary - prints binary representation of number
  * @n: input number
  */
 void print_binary(unsigned long int n)
 {
+	int count = 0, temp = n;
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
+	while (temp)
+	{
+		count++;
+		temp >>= 1;
+	}
 
-	to_binary(n);
+	count--;
+
+	while (count >= 0)
+	{
+		temp = n >> count;
+
+		if (temp & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		count--;
+	}
 }
 
 

27d3b124f8ec927eec36e8c413818ef7ab06b075
Danny Hollman <dannyhollman@gmail.com>
2019-08-08 21:36:59 +0000

    [LOCAL][UPDATE] Change to recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,31 +5,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	int count = 0, temp = n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (temp)
-	{
-		count++;
-		temp >>= 1;
-	}
-
-	count--;
-
-	while (count >= 0)
-	{
-		temp = n >> count;
-
-		if (temp & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-		count--;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
 
 

a6377ed66d81a45eb766c963d9aadc8f520f5558
Danny Hollman <dannyhollman@gmail.com>
2019-08-13 16:55:54 +0000

    [LOCAL][UPDATE] Close and free when needed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,15 +21,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 
 	n = read(fd, buf, letters);
 	if (n == -1)
+	{
+		close(fd);
+		free(buf);
 		return (0);
+	}
+
+	close(fd);
 
 	count = write(1, buf, n);
 	if (count == -1)
+	{
+		free(buf);
 		return (0);
+	}
 
 	free(buf);
 	return (count);

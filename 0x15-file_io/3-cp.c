b2f0aaaaf4c0a02d244d1a8890e622f88c6e6433
Danny Hollman <dannyhollman@gmail.com>
2019-08-13 19:23:35 +0000

    [LOCAL][INIT] Add file 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,64 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * copy_file - copies one file to another
+ * @filename: source file
+ * @new_filename: dest file
+ * Return: 0
+ */
+int copy_file(const char *filename, const char *new_filename)
+{
+	int fd, fd2, rd, wr, cl1, cl2;
+	char buf[1024];
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from %s", filename);
+		exit(98);
+	}
+	fd2 = open(new_filename, O_CREAT | O_TRUNC | O_RDWR, 0664);
+
+	while ((rd = read(fd, buf, 1024)) > 0)
+	{
+		if (rd == -1)
+			exit(99);
+		wr = write(fd2, buf, rd);
+		if (wr == -1)
+		{
+			dprintf(2, "Error: Can't write to %s", new_filename);
+			exit(99);
+		}
+	}
+	cl1 = close(fd);
+	if (cl1 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+	cl2 = close(fd2);
+	if (cl2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d", fd2);
+		exit(100);
+	}
+	return (0);
+}
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to");
+		exit(100);
+	}
+	copy_file(argv[1], argv[2]);
+
+	return (0);
+}

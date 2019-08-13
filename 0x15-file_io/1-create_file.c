b75e0f40cbf972bfd8ade2684532f2b57ad849e5
Danny Hollman <dannyhollman@gmail.com>
2019-08-13 17:45:28 +0000

    [LOCAL][UPDATE] Add trunc flag and change permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len])
 		len++;
 
-	fd = open(filename, O_CREAT | O_RDWR, 0700);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 

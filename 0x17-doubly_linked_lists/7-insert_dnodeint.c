9da586c49de74ec7b1b62f28e3e2d0ec0a29611d
Danny Hollman <dannyhollman@gmail.com>
2019-09-14 23:43:13 +0000

    [LOCAL][UPDATE] Fix order of assignments

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,37 +9,36 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new, *temp;
 
-	temp = *h;
+	dlistint_t *new, *current;
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
+	current = *h;
 	if (idx == 0)
 	{
-		new->n = n;
 		new->prev = NULL;
 		new->next = *h;
 		(*h)->prev = new;
 		*h = new;
-		return (new);
+		return (*h);
 	}
-	while (temp)
+	while (current != NULL)
 	{
-		if (i == (idx - 1))
+		if (i == idx - 1)
 		{
 			new->n = n;
-			new->prev = temp;
-			new->next = temp->next;
-			temp->next = new;
-			temp->next->prev = new;
+			new->next = current->next;
+			current->next = new;
+			new->prev = current;
 			if (new->next != NULL)
 				new->next->prev = new;
 			return (new);
 		}
-		temp = temp->next;
+		current = current->next;
 		i++;
 	}
+	free(new);
 	return (NULL);
 }

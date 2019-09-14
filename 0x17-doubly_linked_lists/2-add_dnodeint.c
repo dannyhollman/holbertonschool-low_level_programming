82fe294e2ec0bb783f6e29c4ecdf69736a637837
Danny Hollman <dannyhollman@gmail.com>
2019-09-14 17:03:25 +0000

    [LOCAL][UPDATE] Change head prev

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,26 +7,16 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	if (*head == NULL)
-	{
-		new->n = n;
-		new->prev = NULL;
-		new->next = NULL;
-		*head = new;
-		return (new);
-	}
-	else
-	{
-		new->n = n;
-		new->prev = NULL;
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }

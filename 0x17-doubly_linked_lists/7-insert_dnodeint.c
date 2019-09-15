766b3a7dcd7b6874a04ee70f0e1d6e405f886243
Danny Hollman <dannyhollman@gmail.com>
2019-09-15 00:13:41 +0000

    [LOCAL][UPDATE] Add NULL condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->n = n;
 		new->prev = NULL;
 		new->next = *h;
-		(*h)->prev = new;
+		if (*h != NULL)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}

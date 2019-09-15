39c5bb71150890b7fd8ee1375186597d75d1bf9b
Danny Hollman <dannyhollman@gmail.com>
2019-09-15 01:40:09 +0000

    [LOCAL][UPDATE] Change conflicting variable name

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,7 +8,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *temp1, *remove;
+	dlistint_t *temp1, *delete;
 
 	temp1 = *head;
 
@@ -25,11 +25,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	for (i = 0; i < index - 1 && temp1; i++)
 		temp1 = temp1->next;
 
-	if (!(temp1->next) || !(temp1))
+	if (temp1->next == NULL || temp1 == NULL)
 		return (-1);
 
-	remove = temp1->next;
-	temp1->next = remove->next;
-	free(remove);
+	delete = temp1->next;
+	temp1->next = delete->next;
+	free(delete);
 	return (1);
 }

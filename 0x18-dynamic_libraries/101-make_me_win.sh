b3fa83b09f921abb4d78e6aea640291942ea9c2e
Danny Hollman <dannyhollman@gmail.com>
2019-09-17 00:05:28 +0000

    [LOCAL][UPDATE] Change download location

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O inject.so https://github.com/dannyhollman/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/inject.so?raw=true
-export LD_PRELOAD=$PWD/inject.so
+wget -q -O /tmp/inject.so https://github.com/dannyhollman/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/inject.so?raw=true
+export LD_PRELOAD=/tmp/inject.so

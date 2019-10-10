3e2d30d2ea157c2a80b3c5a2b8076990ff82e8b4
Danny Hollman <dannyhollman@gmail.com>
2019-10-10 16:42:58 +0000

    [LOCAL][UPDATE] Add free when second malloc fails

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	new = malloc(sizeof(hash_node_t));
@@ -18,7 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new->size = size;
 	new->array = calloc(size, sizeof(hash_node_t));
 	if (new->array == NULL)
+	{
+		free(new);
 		return (NULL);
-
+	}
 	return (new);
 }

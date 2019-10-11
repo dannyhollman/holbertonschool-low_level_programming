94ccdb3bf18ade8da4a1c76ad925ec58897a72b2
Danny Hollman <dannyhollman@gmail.com>
2019-10-11 04:22:04 +0000

    [LOCAL][UPDATE] Remove calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,11 +16,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new->size = size;
-	new->array = calloc(size, sizeof(hash_node_t));
+
+	new->array = malloc(sizeof(hash_node_t) * size);
 	if (new->array == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
+	memset(new->array, 0, sizeof(hash_node_t) * size);
 	return (new);
 }

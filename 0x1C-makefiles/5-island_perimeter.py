7656b4afd480ddb2b3bfc7d92738498980d10db8
Danny Hollman <dannyhollman@gmail.com>
2019-11-16 00:47:53 +0000

    [UPDATE] Check outside rows and columns for island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 """ finds perimeter of island from grid """
 
+
 def island_perimeter(grid):
     """ finds island perimeter """
     perim = 0
-    for row in range(1, len(grid) - 1):
-        for cell in range(1, len(grid[row]) - 1):
+    for row in range(len(grid)):
+        for cell in range(len(grid[row])):
             if grid[row][cell] == 1:
                 # check cell above
-                if grid[row - 1][cell] == 0:
+                if row == 0:
                     perim += 1
+                else:
+                    if grid[row - 1][cell] == 0:
+                        perim += 1
                 # check cell before
-                if grid[row][cell - 1] == 0:
+                if cell == 0:
                     perim += 1
+                else:
+                    if grid[row][cell - 1] == 0:
+                        perim += 1
                 # check cell below
-                if grid[row + 1][cell] == 0:
+                if row == len(grid) - 1:
                     perim += 1
+                else:
+                    if grid[row + 1][cell] == 0:
+                        perim += 1
                 # check cell after
-                if grid[row][cell + 1] == 0:
+                if cell == len(grid[row]) - 1:
                     perim += 1
+                else:
+                    if grid[row][cell + 1] == 0:
+                        perim += 1
     return perim
